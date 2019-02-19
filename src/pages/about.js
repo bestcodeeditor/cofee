3f093787384c33d63498480a3eee1fcfe8a4cfeb
sudip <sudipzoom@rediffmail.com>
2019-02-19 22:02:08 +0530

    about us change

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, graphql } from "gatsby";
 
 import Layout from "../components/layout";
+import Title from "../components/Globals/Title";
 
 import SEO from "../components/seo";
 
@@ -17,7 +18,25 @@ const AboutPage = ({ data }) => (
       title="about us"
       styleClass="about-background"
     />
-    <Info />
+    <section className="py-5">
+      <div className="container">
+        <Title title="about us" />
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Regular Taste becomes a great choice for our visitors to hangout.
+              Regular Taste has established on the belief that our visitors
+              should feel happy with our dishes.
+            </p>
+            <Link to="/">
+              <button className="btn text-uppercase btn-yellow">
+                home page
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 );
 
